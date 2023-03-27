cb91349f074f9cea25d771f370c9743722aa89dd
Frankenstone <dampf_acc@online.de>
2023-03-27 11:52:17 +0200

    libarmbox: add VIDEO_STD_2160P60, libduckbox: add VIDEO_STD_1080P60

--- libduckbox/video.cpp
+++ libduckbox/video.cpp
@@ -146,6 +146,7 @@ static const char *vid_modes[] =
 	"1080p25",  // VIDEO_STD_1080P25
 	"1080p30",  // VIDEO_STD_1080P30
 	"1080p50",  // VIDEO_STD_1080P50
+	"1080p60",  // VIDEO_STD_1080P60
 	"720p50",   // VIDEO_STD_AUTO
 	NULL
 };
